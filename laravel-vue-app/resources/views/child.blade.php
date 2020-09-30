33c0d1f27f2c7d9fda06b45bf747b30f3f60017c
Yuuki883 <yuukisaitou14@gmail.com>
2020-09-30 15:11:27 +0900

    add: ログイン・登録機能追加

---
+++ laravel-vue-app/resources/views/child.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+  <section>
+    <h2>以下はVueコンポーネントです</h2>
+    <foo-bar></foo-bar>
+  </section>
+@endsection
---
+++ part2/src/book_log.php
@@ -0,0 +1,4 @@
+<?php
+
+echo '書籍名:坊ちゃん' . PHP_EOL;
+echo '著者名:夏目漱石' . PHP_EOL;
